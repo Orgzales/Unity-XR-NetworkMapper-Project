b44fc83f899324503256292c5b9c2c3513526cea
Orgzales <77345323+Orgzales@users.noreply.github.com>
2023-10-03 14:30:46 -0400

    UI Text for Wifi Connection Updates

--- AR_Cybersecuity_Project/Assets/Scripts/Checking_Internet.cs
+++ AR_Cybersecuity_Project/Assets/Scripts/Checking_Internet.cs
@@ -2,29 +2,39 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
+using UnityEngine.UI;
 
 public class Checking_Internet : MonoBehaviour
 {
     // Start is called before the first frame update
+
+
+    public Text Wifi_is_Available;
     void Start()
     {
-        if (Application.internetReachability == NetworkReachability.NotReachable)
-        {
-            Debug.Log("No Networks in Area");
-        }
-        else if (Application.internetReachability == NetworkReachability.ReachableViaLocalAreaNetwork)
-        {
-            Debug.Log("Wifi Networks are Availiable");
-        }
-        else if (Application.internetReachability == NetworkReachability.ReachableViaCarrierDataNetwork)
-        {
-            Debug.Log("Network/Wifi Found through Mobile Accounts, Please log in"); //FOR RESNET
-        }
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Wifi_is_Available.text = "Testing";
 
+        if (Application.internetReachability == NetworkReachability.NotReachable)
+        {
+            Wifi_is_Available.text = "No Networks in Area";
+            // Debug.Log("No Networks in Area");
+        }
+        else if (Application.internetReachability == NetworkReachability.ReachableViaLocalAreaNetwork)
+        {
+            Wifi_is_Available.text = "Wifi Networks are Availiable";
+            // Debug.Log("Wifi Networks are Availiable");
+        }
+        else if (Application.internetReachability == NetworkReachability.ReachableViaCarrierDataNetwork)
+        {
+            Wifi_is_Available.text = "Network/Wifi Found through Mobile Accounts, Please log in";
+            // Debug.Log("Network/Wifi Found through Mobile Accounts, Please log in"); //FOR RESNET
+        }
     }
 }
