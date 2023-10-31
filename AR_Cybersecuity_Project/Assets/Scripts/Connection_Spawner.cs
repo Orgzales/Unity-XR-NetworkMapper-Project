07350c41e6462e015c6db731e4a4258de76db74e
Orgzales <77345323+Orgzales@users.noreply.github.com>
2023-10-31 14:25:40 -0400

    Update Connection_Spawner.cs

--- AR_Cybersecuity_Project/Assets/Scripts/Connection_Spawner.cs
+++ AR_Cybersecuity_Project/Assets/Scripts/Connection_Spawner.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 public class Connection_Spawner : MonoBehaviour
 {
-    public GameObject parentObject; // Reference to the parent object
-    public GameObject[] prefabToInstantiate; // The prefab you want to instantiate
+    public GameObject parentObject; //Parent
+    public GameObject[] prefabToInstantiate; //Child
     public float spawnHeight = 0.3f;
+    public float checkRadius = 0.3f;
 
 
     // Start is called before the first frame update
@@ -24,29 +25,51 @@ public class Connection_Spawner : MonoBehaviour
         // }
     }
 
-    void InstantiateChildObject()
+    private void InstantiateChildObject()
     {
-
-        int randomValue = Random.Range(1, 4);
-
-        // Check if the parent object and prefab are set
-        if (parentObject != null && prefabToInstantiate != null)
+        if (CanInstantiateHere()) //Checking if Object of prefab is near.
         {
-            Vector3 cameraPosition = Camera.main.transform.position;
+            int randomValue = Random.Range(1, 4); //Change later based on dBm Value
 
-            // Calculate the position to spawn the object
-            Vector3 spawnPosition = new Vector3(cameraPosition.x, cameraPosition.y - spawnHeight, cameraPosition.z);
+            // Check if the parent object and prefab are set
+            if (parentObject != null && prefabToInstantiate != null)
+            {
+                Vector3 cameraPosition = Camera.main.transform.position;
 
-            // Instantiate the prefab and set the parent to the parentObject
-            GameObject newObject = Instantiate(prefabToInstantiate[randomValue - 1], spawnPosition, Quaternion.identity);
+                // Calculate the position to spawn the object
+                Vector3 spawnPosition = new Vector3(cameraPosition.x, cameraPosition.y - spawnHeight, cameraPosition.z);
 
-            // Set the parent of the new object to the parentObject
-            newObject.transform.SetParent(parentObject.transform);
+                // Instantiate the prefab and set the parent to the parentObject
+                GameObject newObject = Instantiate(prefabToInstantiate[randomValue - 1], spawnPosition, Quaternion.identity);
 
+                // Set the parent of the new object to the parentObject
+                newObject.transform.SetParent(parentObject.transform);
+
+            }
+            else
+            {
+                Debug.LogError("Parent object or prefab not set!");
+            }
+        }
+    }
+
+    private bool CanInstantiateHere()
+    {
+        // Get the position of the main camera
+        Vector3 cameraPosition = Camera.main.transform.position;
+        Vector3 checkPosition = new Vector3(cameraPosition.x, cameraPosition.y - spawnHeight, cameraPosition.z);
+
+        // Check for colliders in the specified radius
+        Collider[] colliders = Physics.OverlapSphere(checkPosition, checkRadius);
+
+        if (colliders.Length == 0)
+        {
+            return true;
         }
         else
         {
-            Debug.LogError("Parent object or prefab not set!");
+            Debug.Log("ANother object too Close!");
+            return false;
         }
     }
 
